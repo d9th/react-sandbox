03dddffe8ab8901d067b1db9e679bdef50efd2dd
d9th <gravitate.karma+github@gmail.com>
2025-02-28 10:53:08 +0900

    モーダルコンポーネントを追加
    新しいモーダルコンポーネントを作成

---
+++ src/components/modal/Modal.tsx
@@ -0,0 +1,33 @@
+import { useRef } from "react";
+
+const Modal = () => {
+  const modalRef = useRef<HTMLDialogElement>(null);
+  const openModal = () => {
+    if (modalRef.current) {
+      modalRef.current.showModal();
+    }
+  };
+
+  return (
+    <>
+      <button className="btn btn-neutral" onClick={openModal}>
+        open modal
+      </button>
+      <dialog className="modal" ref={modalRef}>
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Hello!</h3>
+          <p className="py-4">
+            Press ESC key or click the button below to close
+          </p>
+          <div className="modal-action">
+            <form method="dialog">
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+    </>
+  );
+};
+
+export default Modal;
